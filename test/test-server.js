1e3d574f60fa5aed0b3d9ee9a28e6f70f2284a85
Jamie Davella <jamie.davella@gmail.com>
2016-11-18 08:38:35 -0500

    add require for chai in file

--- test/test-server.js
+++ test/test-server.js
@@ -1,3 +1,14 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server.js')
+
+let should = chai.should();
+let app = server.app;
+let storage = server.storage
+
+chai.use(chaiHttp);
+
+
 describe('Shopping List', function() {
     it('should list items on get');
     it('should add an item on post');
